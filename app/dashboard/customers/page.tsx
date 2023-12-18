a707f3c51cd54a358e619711f225caea724fe704
ahsankhan87 <ahsankhan50@gmail.com>
2023-12-18 21:04:13 +0500

    new

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
+export default function Page() {
+    return (
+        <div className="w-full">
+            <p>customers</p>
+
+        </div>
+    );
+}
