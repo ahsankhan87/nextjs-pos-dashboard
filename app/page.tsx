0d4c4a170ca8927a6af0a903b86c86b204be0dba
ahsankhan87 <ahsankhan50@gmail.com>
2023-12-28 21:50:32 +0500

    logo changed

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,8 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to khybersoft.com.</strong> This is the admin dashboard for maintaining the main application
+
           </p>
           <Link
             href="/login"
